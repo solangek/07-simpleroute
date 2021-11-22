e84614dd8447b211d1a29cf1422e8498f96f72d2
Sol <sodanabe@gmail.com>
2021-11-22 11:13:50 +0200

    cosmetics

--- app-json.js
+++ app-json.js
@@ -2,10 +2,12 @@ const http = require('http');
 const server = http.createServer( (req, res) => {
     console.log(req.url, req.method, req.headers);
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        name: 'John Doe',
+    let myobj = {
+        name: 'John',
+        job: 'Dev',
         age: 30
-    }));
+    };
+    res.end(JSON.stringify(myobj)); // must convert to String before sending
 });
 
 server.listen(3000);
